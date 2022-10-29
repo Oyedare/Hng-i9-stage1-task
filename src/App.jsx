96e7cadf9a837904313836953b4b52025abd5706
Oyedare <ooyedaretemiloluwa@gmail.com>
2022-10-29 08:11:09 +0100

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import profile from './assets/profile__img.svg'
 import ShareBtn from './assets/share-btn'
 import Slack from './assets/slack'
 import Github from './assets/github'
+import zuri from './assets/Zuri.Internship_Logo.svg'
+import I4G from './assets/I4G.svg'
 function App() {
   const links = [
     {
@@ -37,33 +39,41 @@ function App() {
     }
   ]
   return (
-    <div className="container">
-      <div className="main_section">
-        <div className="profile_section">
-          <img src={profile} alt="profile pic" id='profile__img'/>
-          <h1 id='twitter'>Annette Black</h1>
-          <p id='slack'>Oyedare Temiloluwa Stephen</p>
-          <div className="share-icon">
-            <ShareBtn />
+    <>
+      <div className="container">
+        <div className="main_section">
+          <div className="profile_section">
+            <img src={profile} alt="profile pic" id='profile__img'/>
+            <h1 id='twitter'>Annette Black</h1>
+            <p id='slack'>Oyedare Temiloluwa Stephen</p>
+            <div className="share-icon">
+              <ShareBtn />
+            </div>
+          </div>
+
+          <div className="link_section">
+            {links.map(({text,href,id})=>(
+              <a href={href} id={id} target='_blank' rel="noreferrer">{text}</a>
+            ))}
           </div>
         </div>
 
-        <div className="link_section">
-          {links.map(({text,href,id})=>(
-            <a href={href} id={id} target='_blank' rel="noreferrer">{text}</a>
-          ))}
+        <div className="social_section">
+          <a href="https://app.slack.com/client/T042F7V19Q8/D048TLLKCUR/rimeto_profile/U048BSK250D?cdn_fallback=2" target='_blank' rel="noreferrer">
+            <Slack />
+          </a>
+          <a href="https://github.com/Oyedare" target='_blank' rel="noreferrer">
+            <Github />
+          </a>
         </div>
       </div>
 
-      <div className="social_section">
-        <a href="https://app.slack.com/client/T042F7V19Q8/D048TLLKCUR/rimeto_profile/U048BSK250D?cdn_fallback=2" target='_blank' rel="noreferrer">
-          <Slack />
-        </a>
-        <a href="https://github.com/Oyedare" target='_blank' rel="noreferrer">
-          <Github />
-        </a>
+      <div className="footer">
+        <img src={zuri} alt="zuri logo" />
+        <p>HNG Internship 9 Frontend Task</p>
+        <img src={I4G} alt="I4G logo" />
       </div>
-    </div>
+    </>
   );
 }
 
